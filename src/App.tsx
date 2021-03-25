8e31238b883891a0bd8ae85c60e72033f4558aea
Raby Yuson <raby.yuson@firstlook.org>
2021-03-25 14:33:31 -0400

    Filter cdc data by state

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import fetch from 'cross-fetch'
+import usStates from 'states-us'
 import React from 'react'
 
-const URL = 'https://data.cdc.gov/resource/saz5-9hgg.json?$$app_token=9UKsqjohDZQ5pRVegZbpkfSww'
+const CONFIG = {
+  cdc_base_url: 'https://data.cdc.gov/resource/',
+  app_token: '9UKsqjohDZQ5pRVegZbpkfSww',
+  dataset_identifiers: {
+    vaccine_allocation_distributions: {
+      janssen: 'w9zu-fywh',
+      moderna: 'b7pe-5nws',
+      pfizer: 'saz5-9hgg',
+    }
+  }
+}
 
-async function getCdcData() {
-  return await fetch(URL) 
+async function getCdcData({
+  datasetIdentifer,
+  state,
+}: {
+  datasetIdentifer: string;
+  state: string;
+}) {
+  const {
+    cdc_base_url,
+    app_token,
+  } = CONFIG
+  const requestUrl = `${cdc_base_url}${datasetIdentifer}.json?$$app_token=${app_token}&jurisdiction=${state}`
+
+  return await fetch(requestUrl) 
     .then(response => {
       if (response.status >= 400) {
         throw new Error("Bad response from server")
@@ -25,15 +48,46 @@ class App extends React.Component<{}, any> {
   }
 
   componentDidMount() {
-    getCdcData()
-      .then(cdcData => this.setState({ cdcData }))
+    
   }
 
   render() {
     const { cdcData } = this.state
+    const { dataset_identifiers } = CONFIG
+    const { vaccine_allocation_distributions } = dataset_identifiers
+    const {
+      janssen,
+      moderna,
+      pfizer,
+    } = vaccine_allocation_distributions
 
     return (
       <div className="App">
+        <select
+          onChange={(event) => {
+            const selectedState = event.target.value
+            getCdcData({
+              datasetIdentifer: pfizer,
+              state: selectedState,
+            })
+              .then(cdcData => this.setState({ cdcData }))
+          }}
+        >
+          <option>- - Choose a state - -</option>
+          {
+            usStates.map((state, index) => {
+              return (
+                <option
+                  key={index}
+                  value={state.name}
+                >
+                  {state.name}
+                </option>
+              )
+            })
+          }
+        </select>
+
         {cdcData.map((data: {
           jurisdiction: string;
           week_of_allocations: string;
@@ -41,7 +95,6 @@ class App extends React.Component<{}, any> {
           _2nd_dose_allocations: string;
         }, index: number) => {
           const date = new Date(data.week_of_allocations)
-          console.log(date)
 
           return (
             <div key={index}>
