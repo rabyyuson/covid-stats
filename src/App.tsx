6b8e54185d65f2cb0f37559bb4ca9b22c2b5ac3e
Raby Yuson <raby.yuson@firstlook.org>
2021-03-24 23:18:08 -0400

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import fetch from 'cross-fetch'
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const URL = 'https://data.cdc.gov/resource/saz5-9hgg.json?$$app_token=9UKsqjohDZQ5pRVegZbpkfSww'
+
+async function getCdcData() {
+  return await fetch(URL) 
+    .then(response => {
+      if (response.status >= 400) {
+        throw new Error("Bad response from server")
+      }
+      return response.json()
+    })
+    .catch(error => {
+      console.error(error)
+    })
+}
+
+class App extends React.Component {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      cdcData: {
+        jurisdiction: "",
+        week_of_allocations: "",
+        _1st_dose_allocations: "",
+        _2nd_dose_allocations: "",
+      }
+    }
+  }
+
+  componentDidMount() {
+    const cdcData = getCdcData()
+    cdcData.then(data => console.log(data))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Content here
+      </div>
+    );
+  }
 }
 
 export default App;
