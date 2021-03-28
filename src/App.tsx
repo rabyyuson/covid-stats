fd71a3897ed6cd1705335f0f6610bc64a958d2fa
Raby Yuson <raby.yuson@firstlook.org>
2021-03-28 17:44:32 -0400

    Fix issues with destroying chart

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,16 @@ const CONFIG = {
   socrata: {
     app_token: '9UKsqjohDZQ5pRVegZbpkfSww',
     cdc_base_url: 'https://data.cdc.gov/resource/',
+    healthdata_base_url: 'https://healthdata.gov/resource/',
     dataset_identifiers: {
       cases_and_deaths_by_state_over_time: '9mfq-cb36',
+      diagnostic_laboratory_polymerase_chain_reaction_testing: 'j8mb-icvb',
+      patient_impact_and_hospital_capacity: 'g62h-syeh',
     }
   }
 }
 
-interface caseAndDeath {
+interface dataItem {
   [key: string]: string;
 }
 
@@ -43,40 +46,55 @@ async function fetchJSONData({
 }
 
 class App extends React.Component<{}, any> {
+  confirmedHospitalizedAdultsRef: React.RefObject<HTMLCanvasElement>
+  confirmedHospitalizedChildrenRef: React.RefObject<HTMLCanvasElement>
   confirmedCasesRef: React.RefObject<HTMLCanvasElement>
-  confirmedCasesChart: Chart | undefined
-
+  confirmedDeathsRef: React.RefObject<HTMLCanvasElement>
   newCasesRef: React.RefObject<HTMLCanvasElement>
-  newCasesChart: Chart | undefined
-
   newDeathsRef: React.RefObject<HTMLCanvasElement>
-  newDeathsChart: Chart | undefined
+  polymeraseChainReactionPositiveRef: React.RefObject<HTMLCanvasElement>
+  polymeraseChainReactionNegativeRef: React.RefObject<HTMLCanvasElement>
 
   constructor(props: any) {
     super(props)
 
+    this.confirmedHospitalizedAdultsRef = React.createRef()
+    this.confirmedHospitalizedChildrenRef = React.createRef()
     this.confirmedCasesRef = React.createRef()
+    this.confirmedDeathsRef = React.createRef()
     this.newCasesRef = React.createRef()
     this.newDeathsRef = React.createRef()
+    this.polymeraseChainReactionPositiveRef = React.createRef()
+    this.polymeraseChainReactionNegativeRef = React.createRef()
 
     this.handleOnChange = this.handleOnChange.bind(this)
-    this.fetchCdcData = this.fetchCdcData.bind(this)
+    this.fetchData = this.fetchData.bind(this)
     this.setChart = this.setChart.bind(this)
 
     this.state = {
       cdcData: {
         casesAndDeathsByStateOverTime: [],
-      }
+      },
+      healthdata: {
+        diagnosticLaboratoryPolymeraseChainReactionTesting: {},
+        patientImpactAndHospitalCapacity: []
+      },
+      charts: {}
     }
   }
 
-  fetchCdcData(state: string) {
+  fetchData(state: string) {
     const { socrata } = CONFIG
     const {
       cdc_base_url,
+      healthdata_base_url,
       dataset_identifiers,
     } = socrata
-    const { cases_and_deaths_by_state_over_time } = dataset_identifiers
+    const {
+      cases_and_deaths_by_state_over_time,
+      diagnostic_laboratory_polymerase_chain_reaction_testing,
+      patient_impact_and_hospital_capacity,
+    } = dataset_identifiers
 
     const [
       stateName,
@@ -86,107 +104,233 @@ class App extends React.Component<{}, any> {
     const today = new Date()
     const month = today.getMonth()
     const lastMonth = month ? month - 1 : month
+    const dayLastMonth = `${format(new Date(today.getFullYear(), lastMonth), 'yyyy-MM-01')}T00:00:00.000`
 
     Promise.all([
+      // Cases and deaths
       fetchJSONData({
         baseUrl: cdc_base_url,
         datasetIdentifier: cases_and_deaths_by_state_over_time,
-        filters: `&state=${stateAbbreviation}&$order=submission_date ASC&$where=submission_date >= '${format(new Date(today.getFullYear(), lastMonth), 'yyyy-MM-01')}T00:00:00.000'`,
+        filters: `&state=${stateAbbreviation}&$order=submission_date ASC&$where=submission_date >= '${dayLastMonth}'`,
+      }),
+
+      // Patient impact and hospital capacity
+      fetchJSONData({
+        baseUrl: healthdata_base_url,
+        datasetIdentifier: patient_impact_and_hospital_capacity,
+        filters: `&state=${stateAbbreviation}&$order=date ASC&$where=date >= '${dayLastMonth}'`,
+      }),
+
+      // Polymerase Chain Reaction Testing - Positive
+      fetchJSONData({
+        baseUrl: healthdata_base_url,
+        datasetIdentifier: diagnostic_laboratory_polymerase_chain_reaction_testing,
+        filters: `&overall_outcome=Positive&state=${stateAbbreviation}&$order=date ASC&$where=date >= '${dayLastMonth}'`,
+      }),
+
+      // Polymerase Chain Reaction Testing - Negative
+      fetchJSONData({
+        baseUrl: healthdata_base_url,
+        datasetIdentifier: diagnostic_laboratory_polymerase_chain_reaction_testing,
+        filters: `&overall_outcome=Negative&state=${stateAbbreviation}&$order=date ASC&$where=date >= '${dayLastMonth}'`,
       }),
     ])
       .then(data => {
+        // console.log(data)
         this.setState({
           cdcData: {
             casesAndDeathsByStateOverTime: data[0],
-            vaccineAllocationDistributions: {
-              janssen: data[1],
-              moderna: data[2],
-              pfizer: data[3],
-            }
+          },
+          healthdata: {
+            patientImpactAndHospitalCapacity: data[1],
+            diagnosticLaboratoryPolymeraseChainReactionTesting: {
+              positive: data[2],
+              negative: data[3],
+            },
           }
+        })
+
+        const {
+          cdcData,
+          healthdata,
+        } = this.state
+        const { casesAndDeathsByStateOverTime } = cdcData
+        const {
+          diagnosticLaboratoryPolymeraseChainReactionTesting,
+          patientImpactAndHospitalCapacity
+        } = healthdata
+        const {
+          positive: polymeraseChainReactionPositive,
+          negative: polymeraseChainReactionNegative,
+        } = diagnosticLaboratoryPolymeraseChainReactionTesting
+
+        if (
+          !casesAndDeathsByStateOverTime.length ||
+          !patientImpactAndHospitalCapacity.length ||
+          !polymeraseChainReactionPositive.length ||
+          !polymeraseChainReactionNegative.length
+        ) {
+          return
+        }
+
+        // Polymerase Chain Reaction Positive
+        this.setChart({
+          id: 'polymeraseChainReactionPositive',
+          chartType: 'bar',
+          color: 'rgb(242,57,57,0.75)',
+          data: polymeraseChainReactionPositive,
+          filter: {
+            date: 'date',
+            identifier: 'total_results_reported',
+          },
+          label: 'Polymerase Chain Reaction (PCR) Testing - Positive',
+          ref: this.polymeraseChainReactionPositiveRef,
+        })
+
+        // Polymerase Chain Reaction Negative
+        this.setChart({
+          id: 'polymeraseChainReactionNegative',
+          chartType: 'bar',
+          color: 'rgb(242,57,57,0.75)',
+          data: polymeraseChainReactionNegative,
+          filter: {
+            date: 'date',
+            identifier: 'total_results_reported',
+          },
+          label: 'Polymerase Chain Reaction (PCR) Testing - Negative',
+          ref: this.polymeraseChainReactionNegativeRef,
+        })
+
+        // Confirmed Hospitalized Adults
+        this.setChart({
+          id: 'confirmedHospitalizedAdults',
+          chartType: 'line',
+          color: 'rgb(242,57,57,0.75)',
+          data: patientImpactAndHospitalCapacity,
+          filter: {
+            date: 'date',
+            identifier: 'total_adult_patients_hospitalized_confirmed_covid',
+          },
+          label: 'Confirmed Hospitalized Adults',
+          ref: this.confirmedHospitalizedAdultsRef,
+        })
+
+
+        // Confirmed Hospitalized Children
+        this.setChart({
+          id: 'confirmedHospitalizedChildren',
+          chartType: 'line',
+          color: 'rgb(242,57,57,0.75)',
+          data: patientImpactAndHospitalCapacity,
+          filter: {
+            date: 'date',
+            identifier: 'total_pediatric_patients_hospitalized_confirmed_covid',
+          },
+          label: 'Confirmed Hospitalized Children',
+          ref: this.confirmedHospitalizedChildrenRef,
         })
 
         // Confirmed Cases
         this.setChart({
-          filter: 'conf_cases',
-          ref: this.confirmedCasesRef,
-          color: '#f23939',
-          chart: this.confirmedCasesChart,
-          chartType: 'horizontalBar',
-          label: 'Confirmed Cases',
-        })
-
-        // New Cases
-        this.setChart({
-          filter: 'new_case',
-          ref: this.newCasesRef,
-          color: '#f23939',
-          chart: this.newCasesChart,
+          id: 'confirmedCases',
           chartType: 'bar',
-          label: 'New Cases',
+          color: 'rgb(242,57,57,0.75)',
+          data: casesAndDeathsByStateOverTime,
+          filter: {
+            date: 'submission_date',
+            identifier: 'conf_cases',
+          },
+          label: 'Confirmed Cases',
+          ref: this.confirmedCasesRef,
         })
 
         // Confirmed Deaths
         this.setChart({
-          filter: 'new_death',
-          ref: this.newDeathsRef,
-          color: '#f23939',
-          chart: this.newDeathsChart,
+          id: 'confirmedDeaths',
+          chartType: 'bar',
+          color: 'rgb(242,57,57,0.75)',
+          data: casesAndDeathsByStateOverTime,
+          filter: {
+            date: 'submission_date',
+            identifier: 'conf_death',
+          },
+          label: 'Confirmed Deaths',
+          ref: this.confirmedDeathsRef,
+        })
+
+        // New Cases
+        this.setChart({
+          id: 'newCases',
           chartType: 'line',
+          color: 'rgb(242,57,57,0.75)',
+          data: casesAndDeathsByStateOverTime,
+          filter: {
+            date: 'submission_date',
+            identifier: 'new_case',
+          },
+          label: 'New Cases',
+          ref: this.newCasesRef,
+        })
+
+        // Confirmed Deaths
+        this.setChart({
+          id: 'newDeaths',
+          chartType: 'line',
+          color: 'rgb(242,57,57,0.75)',
+          data: casesAndDeathsByStateOverTime,
+          filter: {
+            date: 'submission_date',
+            identifier: 'new_death',
+          },
           label: 'New Deaths',
+          ref: this.newDeathsRef,
         })
       })
   }
 
   setChart({
-    filter,
-    ref,
-    label,
-    color,
-    chart,
+    id,
     chartType,
+    color,
+    data,
+    filter,
+    label,
+    ref,
   }:
   {
-    filter: string;
-    ref: React.RefObject<HTMLCanvasElement>;
-    label: string;
-    color: string;
-    chart: Chart | undefined,
+    id: string;
     chartType: string;
+    color: string;
+    data: [dataItem],
+    filter: {
+      date: string;
+      identifier: string;
+    };
+    label: string;
+    ref: React.RefObject<HTMLCanvasElement>;
   }) {
-    const { cdcData } = this.state
-    const { casesAndDeathsByStateOverTime } = cdcData
-    if (!filter || !casesAndDeathsByStateOverTime.length || !ref) {
-      return
+    const { charts } = this.state
+    const existingChart = charts[id]
+    if (existingChart) {
+      existingChart.destroy()
     }
 
-    if (chart) {
-      chart.destroy()
-    }
-
-    const recentCases = casesAndDeathsByStateOverTime.slice(-7)
-    const cases = recentCases.map((
-      caseItem: caseAndDeath
-    ) => ({
-      submissionDate: caseItem.submission_date,
-      [filter]: caseItem[filter],
-    }))
-
+    const recentData = data.slice(-7)
     const canvasElement = ref.current || ''
-    chart = new Chart(canvasElement, {
+    const newChart = new Chart(canvasElement, {
       type: chartType,
       data: {
-        labels: cases.map((
-          caseItem: caseAndDeath
-        ) => format(new Date(caseItem.submissionDate), 'MM/dd')),
+        labels: recentData.map((
+          dataItem: dataItem
+        ) => format(new Date(dataItem[filter.date]), 'MM/dd')),
         datasets: [
           {
             label,
             backgroundColor: color,
             borderColor: color,
-            data: cases.map((
-              caseItem: caseAndDeath
-            ) => caseItem[filter]),
+            data: recentData.map((
+              dataItem: dataItem
+            ) => Number(dataItem[filter.identifier])),
             fill: false
           }
         ]
@@ -195,25 +339,56 @@ class App extends React.Component<{}, any> {
         maintainAspectRatio: false,
       }
     })
+
+    charts[id] = newChart
+    this.setState({
+      charts,
+      ...this.state
+    })
   }
 
   handleOnChange(state: string) {
-    this.fetchCdcData(state)
+    this.fetchData(state)
   }
 
   render() {
-    const { cdcData } = this.state
+    const {
+      cdcData,
+      healthdata,
+    } = this.state
     const { casesAndDeathsByStateOverTime } = cdcData
+    const {
+      diagnosticLaboratoryPolymeraseChainReactionTesting,
+      patientImpactAndHospitalCapacity,
+    } = healthdata
+    const {
+      positive: polymeraseChainReactionPositive,
+      negative: polymeraseChainReactionNegative,
+    } = diagnosticLaboratoryPolymeraseChainReactionTesting
+
+    const headingStyle = {
+      margin: '0 0 20px 0',
+    }
+    const stateSelectStyles = {
+      fontSize: '18px',
+      padding: '10px',
+      FontWeight: 'bold',
+    }
+    const canvasWrapperStyles = {
+      display: 'flex',
+      FlexDirection: 'row',
+    }
+    const canvasStyles = {
+      Position: 'relative',
+      width: '500px',
+      height: '300px',
+    }
 
     return (
       <div className="App">
-        <h1>Covid-19 Statistics</h1>
+        <h1 style={headingStyle}>Covid-19 Statistics</h1>
         <select
-          style={{
-            fontSize: '18px',
-            padding: '20px',
-            fontWeight: 'bold',
-          }}
+          style={stateSelectStyles}
           onChange={(event) => this.handleOnChange(event.target.value)}
         >
           <option>- - Choose a state - -</option>
@@ -231,117 +406,66 @@ class App extends React.Component<{}, any> {
           }
         </select>
         
+        {(
+          (polymeraseChainReactionPositive && Boolean(polymeraseChainReactionPositive.length)) && 
+          (polymeraseChainReactionNegative && Boolean(polymeraseChainReactionNegative.length))
+        ) && (
+          <>
+            <br/><br/><br/>
+            <div style={canvasWrapperStyles}>
+              <div style={canvasStyles}>
+                <canvas ref={this.polymeraseChainReactionPositiveRef} />
+              </div>
+              
+              <div style={canvasStyles}>
+                <canvas ref={this.polymeraseChainReactionNegativeRef} />
+              </div>
+            </div>
+          </>
+        )}
+
+        {Boolean(patientImpactAndHospitalCapacity.length) && (
+          <>
+            <br/><br/><br/>
+            <div style={canvasWrapperStyles}>
+              <div style={canvasStyles}>
+                <canvas ref={this.confirmedHospitalizedAdultsRef} />
+              </div>
+
+              <div style={canvasStyles}>
+                <canvas ref={this.confirmedHospitalizedChildrenRef} />
+              </div>
+            </div>
+          </>
+        )}
+
         {Boolean(casesAndDeathsByStateOverTime.length) && (
           <>
             <br/><br/><br/>
-            <div
-              style={{
-                width: '100%',
-              }}
-            >
-              <div style={{
-                position: 'relative',
-                width: '400px',
-                height: '300px',
-                display: 'inline-block',
-              }}>
+            <div style={canvasWrapperStyles}>
+              <div style={canvasStyles}>
                 <canvas ref={this.confirmedCasesRef} />
               </div>
-
-              <div style={{
-                position: 'relative',
-                width: '400px',
-                height: '300px',
-                display: 'inline-block',
-              }}>
-                <canvas ref={this.newCasesRef} />
-              </div>
               
-              <div style={{
-                position: 'relative',
-                width: '400px',
-                height: '300px',
-                display: 'inline-block',
-              }}>
-                <canvas ref={this.newDeathsRef} />
+              <div style={canvasStyles}>
+                <canvas ref={this.confirmedDeathsRef} />
               </div>
             </div>
 
-            <br/><br/>
-            {this.renderCasesAndDeaths(casesAndDeathsByStateOverTime)}
+            <br/><br/><br/>
+            <div style={canvasWrapperStyles}>
+              <div style={canvasStyles}>
+                <canvas ref={this.newCasesRef} />
+              </div>
+              
+              <div style={canvasStyles}>
+                <canvas ref={this.newDeathsRef} />
+              </div>
+            </div>
           </>
         )}
       </div>
     );
-  }
-
-  renderCasesAndDeaths(casesAndDeathsByStateOverTime: [caseAndDeath]) {
-    return (
-      <>
-        <table>
-          <thead>
-            <tr>
-              <td>Submission date</td>
-              <td>Total number of cases</td>
-              <td>Total confirmed cases</td>
-              <td>Total probable cases</td>
-              <td>Number of new cases</td>
-              <td>Number of new probable cases</td>
-              <td>Total number of deaths</td>
-              <td>Total number of confirmed deaths</td>
-              <td>Total number of probable deaths</td>
-              <td>Number of new deaths</td>
-              <td>Number of new probable deaths</td>
-              <td>Date and time record was created</td>
-              <td>If Agree, then confirmed and probable cases are included. If Not Agree, then only total cases are included.</td>
-              <td>If Agree, then confirmed and probable deaths are included. If Not Agree, then only total deaths are included.</td>
-            </tr>
-          </thead>
-          {
-            casesAndDeathsByStateOverTime.map((data, index) => {
-              const {
-                submission_date,
-                tot_cases,
-                conf_cases,
-                prob_cases,
-                new_case,
-                pnew_case,
-                tot_death,
-                conf_death,
-                prob_death,
-                new_death,
-                pnew_death,
-                created_at,
-                consent_cases,
-                consent_deaths,
-              } = data
-              const submissionDate = format(new Date(submission_date), 'MM/dd/yyyy')
-              const dateAndTimeRecordWasCreated = format(new Date(created_at), 'MM/dd/yyyy')
-              return (
-                <tbody key={index}>
-                  <tr>
-                    <td>{submissionDate}</td>
-                    <td>{tot_cases}</td>
-                    <td>{conf_cases}</td>
-                    <td>{prob_cases}</td>
-                    <td>{new_case}</td>
-                    <td>{pnew_case}</td>
-                    <td>{tot_death}</td>
-                    <td>{conf_death}</td>
-                    <td>{prob_death}</td>
-                    <td>{new_death}</td>
-                    <td>{pnew_death}</td>
-                    <td>{dateAndTimeRecordWasCreated}</td>
-                    <td>{consent_cases}</td>
-                    <td>{consent_deaths}</td>
-                  </tr>
-                </tbody>
-              )
-            })
-          }
-        </table>
-      </>
-    )
   }
 }
 
